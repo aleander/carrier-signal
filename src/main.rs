4032ffcecd4621333f12e38d61ef0cffe336b1f6
ModZero <modzero@modzero.xyz>
2020-06-01 03:28:29 +0200

    Integrate with Warp

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 mod simulation;
 mod state;
 
+use std::convert::Infallible;
+
+use tokio::sync::watch;
+use warp::Filter;
+
 use simulation::Simulation;
+use state::State;
 
 #[tokio::main]
 async fn main() {
     let mut simulation = Simulation::new();
-    let mut state_channel = simulation.state();
-    
-    tokio::spawn(async move {
+    let state_channel = simulation.state();
+
+    let sim = tokio::spawn(async move {
         simulation.run().await;
     });
 
-    loop {
-        let state = state_channel.recv().await.unwrap();
-        println!("Iteration {}", state.iteration)
-    }
+    let hello = warp::path!("hello" / String)
+        .map(move |name| { (name, state_channel.clone()) })
+        .and_then(|(name, channel)| async move { hello(name, channel).await });
+       
+    let server = tokio::spawn(async move { warp::serve(hello).run(([127, 0, 0, 1], 3030)).await });
+
+    tokio::try_join!(sim, server).unwrap();
+}
+
+async fn hello(
+    name: String,
+    mut state_channel: watch::Receiver<State>,
+) -> Result<impl warp::Reply, Infallible> {
+    Ok(format!(
+        "Hello, {}! We're at iteration {}",
+        name,
+        state_channel.recv().await.unwrap().iteration
+    ))
 }
