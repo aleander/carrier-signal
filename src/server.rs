000846060c4b5098ae869d2044381e71cededce4
ModZero <modzero@modzero.xyz>
2020-08-23 00:40:46 +0200

    Remove Tera usage, the client is its own thing now

--- src/server.rs
+++ src/server.rs
@@ -1,44 +1,21 @@
-use std::{sync::Arc, time::Duration};
+use std::time::Duration;
 
 use futures::{SinkExt, StreamExt};
 use serde_json::json;
-use tera::{Context, Tera};
 use tokio::{sync::watch, time::throttle};
 use warp::{
     reject::Reject,
     ws::{Message, WebSocket},
-    Filter, Rejection,
+    Filter,
 };
 
 use crate::state::State;
 
 pub async fn setup_server(state_channel: watch::Receiver<State>) {
-    let tr = match Tera::new("templates/**/*.tera") {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Parsing error(s): {}", e);
-            ::std::process::exit(1);
-        }
-    };
-    let tr = Arc::new(tr);
-    let tera = move || {
-        let tr = tr.clone();
-        move |with_template| render(with_template, tr.clone())
-    };
     let state = move || {
         let state_channel = state_channel.clone();
         move || state_channel.clone()
     };
-
-    let index = warp::get()
-        .and(warp::path::end())
-        .map(state())
-        .and_then(|channel| async move { get_state(channel).await })
-        .map(|state| WithTemplate {
-            name: "stuff.tera",
-            context: Context::from_serialize(state).unwrap(),
-        })
-        .map(tera());
 
     let wsstate = state();
     let ws = warp::path("state")
@@ -48,12 +25,7 @@ pub async fn setup_server(state_channel: watch::Receiver<State>) {
             ws.on_upgrade(move |websocket| watch_state(websocket, state))
         });
 
-    warp::serve(index.or(ws)).run(([127, 0, 0, 1], 3030)).await;
-}
-
-struct WithTemplate {
-    name: &'static str,
-    context: Context,
+    warp::serve(ws).run(([127, 0, 0, 1], 3030)).await;
 }
 
 async fn watch_state(ws: WebSocket, mut state_channel: watch::Receiver<State>) {
@@ -78,18 +50,3 @@ async fn watch_state(ws: WebSocket, mut state_channel: watch::Receiver<State>) {
 struct SimulationUnavailable;
 
 impl Reject for SimulationUnavailable {}
-
-async fn get_state(mut channel: watch::Receiver<State>) -> Result<State, Rejection> {
-    match channel.recv().await {
-        Some(state) => Ok(state),
-        None => Err(warp::reject::custom(SimulationUnavailable)),
-    }
-}
-
-fn render(template: WithTemplate, tera: Arc<Tera>) -> impl warp::Reply {
-    let render = tera
-        .render(template.name, &template.context)
-        .unwrap_or_else(|err| err.to_string());
-
-    warp::reply::html(render)
-}
